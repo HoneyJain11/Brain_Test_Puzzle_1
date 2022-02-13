dbeda1510a0b23e368742d2f6a90f07e8eb95a43
HoneyJain11 <hrjain911@gmail.com>
2022-02-13 13:12:58 +0530

    Added First type of puzzle

---
+++ Assets/Scripts/Level/LevelModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelModel
+{
+    public int levelNo { get; set; }
+    public string levelTitle { get; set; }
+    public Sprite[] levelSpriteElement { get; set; }
+
+    public LevelModel(LevelScriptableObject levelScriptableObject)
+    {
+        levelNo = levelScriptableObject.levelNo;
+        levelTitle = levelScriptableObject.levelTitle;
+        levelSpriteElement = levelScriptableObject.levelSpriteElement;
+    }
+
+    
+
+}
---
+++ Assets/Scripts/LevelScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelScriptableObject", menuName = "CreateNewLevel")]
+public class LevelScriptableObject : ScriptableObject
+{
+  
+    public int levelNo;
+    public string levelTitle;
+    public Sprite[] levelSpriteElement;
+
+}
+
+
